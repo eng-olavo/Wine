37b3c3a1ed5adc440232b1ca3adec075ec5c5662
Olavo-DIO <olavo.melo.br.DIO@gmail.com>
2021-10-08 09:14:33 -0300

    App Wine 1º Commit django setup: -pip install ...., requirements.txt -startproject "wine" e startapp "core" -Settings -Wine: urls -Core: templates, static, urls, views -Core/views: index, adega, adicionarVinho -makemigrations e migrate -createsuperuser: admin, 1234

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, adega, adicionarVinho
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('adega/', adega, name='adega'),
+    path('adicionarVinho/', adicionarVinho, name='adicionarVinho'),
+]
---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+    return render(request,'index.html')
+
+
+def adega(request):
+    return render(request,'adega.html')
+
+
+def adicionarVinho(request):
+    return render(request,'adicionarVinho.html')
